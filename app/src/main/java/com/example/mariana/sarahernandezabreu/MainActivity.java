d82a2daf977de1159b003e6e6f87d38e932f62b2
sarahernandezabreu <nare_k97@hotmail.com>
2017-09-22 00:41:50 -0500

    initial commit

---
+++ app/src/main/java/com/example/mariana/sarahernandezabreu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mariana.sarahernandezabreu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
